fe15de51411fdfea50f0d9e6af82a254e3ee8ef8
We1l6.f <we1l6.f@gmail.com>
2025-02-02 14:34:56 +0200

    fix: main.cpp

--- src/database/Queries.h
+++ src/database/Queries.h
@@ -6,7 +6,8 @@
 namespace Queries {
     const QString gridTable = ("CREATE TABLE IF NOT EXISTS gridTable ("
                                "ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "
-                               "list_id INTEGER PRIMARY KEY NOT NULL, "
+                               "sheet_id INTEGER NOT NULL, "
+                               "row_number INTEGER NOT NULL,"
                                "to_whom_issued VARCHAR(32), "
                                "unit VARCHAR(32), "
                                "account_number VARCHAR(32), "
--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     DatabaseManager::instance().connect("database.db");
-<<<<<<< HEAD
-=======
     DatabaseManager::instance().disconnect();
->>>>>>> 4051ed4ad24336f7ab2eff9e1af3d22d0868d872
     w.show();
     return a.exec();
     DatabaseManager::instance().disconnect();
