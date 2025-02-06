3635fe001ad5306d32994d1fde72deab1319fe9e
We1l6.f <we1l6.f@gmail.com>
2025-02-06 23:36:18 +0200

    added signature save. A lot of unnecessary code

--- src/signatureWindow.h
+++ src/signatureWindow.h
@@ -4,6 +4,9 @@
 #include <QDialog>
 #include <QTimer>
 #include <QKeyEvent>
+#include <QPainter>
+#include <QBuffer>
+
 namespace Ui {
 class SignatureWindow;
 }
@@ -15,6 +18,10 @@ class SignatureWindow : public QDialog
 public:
     explicit SignatureWindow(QWidget *parent = nullptr);
     ~SignatureWindow();
+    void setImageFromByteArray(const QByteArray &byteArray);
+
+    QByteArray getImageAsByteArray();
+    QByteArray byteArray;
 
 private:
     Ui::SignatureWindow *ui;
@@ -23,7 +30,19 @@ private:
     bool drawing;
     QTimer *cursorTimer = nullptr;
 protected:
+    void clearImage();
+    void saveImage(const QString &filePath);
     void keyPressEvent(QKeyEvent *event) override;
+    void paintEvent(QPaintEvent *event) override;
+    void mousePressEvent(QMouseEvent *event) override;
+    void mouseMoveEvent(QMouseEvent *event) override;
+    void mouseReleaseEvent(QMouseEvent *event) override;
+    void resizeEvent(QResizeEvent *event) override;
+    void resizeImage(QImage *image, const QSize &newSize);
+    void closeEvent(QCloseEvent *event) override;
+    //void leaveEvent(QEvent *event) override;
+    //void enterEvent(QEnterEvent *event) override;
+    //void showEvent(QShowEvent *event) override;
 };
 
 #endif // SIGNATUREWINDOW_H
