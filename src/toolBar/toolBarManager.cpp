49cbc1a385021d4f6f2e2ba4d1487bb9676e02ef
We1l6.f <we1l6.f@gmail.com>
2025-02-12 22:51:12 +0200

    feat: created class named ToolBarManager, cteated folder named img

---
+++ src/toolBar/toolBarManager.cpp
@@ -0,0 +1,5 @@
+#include "toolBarManager.h"
+
+ToolBarManager::ToolBarManager(QWidget *parent) : QToolBar(parent) {
+    setObjectName("toolbar");
+}
---
+++ src/toolBar/toolBarManager.h
@@ -0,0 +1,11 @@
+#ifndef TOOLBARMANAGER_H
+#define TOOLBARMANAGER_H
+#include<QToolBar>
+
+class ToolBarManager : public QToolBar
+{
+public:
+    explicit ToolBarManager(QWidget *parent = nullptr);
+};
+
+#endif // TOOLBARMANAGER_H
